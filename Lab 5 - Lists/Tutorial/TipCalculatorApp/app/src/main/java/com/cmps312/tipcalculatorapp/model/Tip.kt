97d80ab0a829d1c37baa85956884b24a97a5e054
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-09-15 14:26:05 +0300

    Update Tip.kt

--- Lab 5 - Lists/Tutorial/TipCalculatorApp/app/src/main/java/com/cmps312/tipcalculatorapp/model/Tip.kt
+++ Lab 5 - Lists/Tutorial/TipCalculatorApp/app/src/main/java/com/cmps312/tipcalculatorapp/model/Tip.kt
@@ -1,5 +1,13 @@
 package com.cmps312.tipcalculatorapp.model
 
+//"Good" "Okay" "Amaizing"
 class Tip(var amount: Double = 0.0, tipOption: String, roundUp : Boolean) {
+
+    //CTRL + D -> repear a line
+    private val tipPercentage = when(tipOption){
+        "Okay (10%)" -> 0.1
+        "Good (10%)" -> 0.15
+        "Amazing (10%)" -> 0.2
+    }
 
 }
