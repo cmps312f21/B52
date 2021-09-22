3eca537ba4557668646620acbce86cfaedb55da5
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-09-22 15:59:52 +0300

    Final Code
    Missing minor adjustments in the layout

--- Lab 6 - Navigations/Qatar2022/app/src/main/java/com/cmps312/myapplication/MainActivity.kt
+++ Lab 6 - Navigations/Qatar2022/app/src/main/java/com/cmps312/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
@@ -11,7 +12,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.cmps312.myapplication.component.SearchBar
+import com.cmps312.myapplication.component.SortByDD
 import com.cmps312.myapplication.component.StadiumList
+import com.cmps312.myapplication.repository.SortBy
 import com.cmps312.myapplication.repository.StadiumRepo
 import com.cmps312.myapplication.ui.theme.MyApplicationTheme
 
@@ -33,23 +36,36 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
     var searchText by remember { mutableStateOf("") }
+    var selectedOption by remember { mutableStateOf("All") }
+
     Scaffold(
         topBar = {
-            SearchBar(searchText, onSearch = {
-                searchText = it
-            })
+            Column() {
+                SearchBar(searchText, onSearch = {
+                    searchText = it
+                })
+                SortByDD(selectedOption, onSelectedOptionChanged = {
+                    selectedOption = it
+                })
+            }
         },
-        content = { Content(searchText) }
+        content = { Content(searchText, selectedOption) }
     )
 }
 
 @Composable
-fun Content(searchText : String) {
-    val stadiums = when(searchText.isNotEmpty()){
+fun Content(searchText: String, sortBy: String) {
+    val stadiums = when (searchText.isNotEmpty()) {
         true -> StadiumRepo.filterStadiumByName(searchText)
         else -> StadiumRepo.getStadiums(LocalContext.current)
     }
-    StadiumList(stadiums = stadiums)
+    val sortedStadiums = when (sortBy) {
+        SortBy.CITY_NAME -> stadiums.sortedBy { it.city }
+        SortBy.SEATING_CAPACITY -> stadiums.sortedBy { it.seatingCapacity }
+        SortBy.STADIUM_NAME -> stadiums.sortedBy { it.name }
+        else -> stadiums
+    }
+    StadiumList(stadiums = sortedStadiums)
 }
 
 @Preview(showBackground = true)
--- Lab 6 - Navigations/Qatar2022/app/src/main/java/com/cmps312/myapplication/component/SortByDD.kt
+++ Lab 6 - Navigations/Qatar2022/app/src/main/java/com/cmps312/myapplication/component/SortByDD.kt
@@ -1,13 +1,47 @@
 package com.cmps312.myapplication.component
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import com.cmps312.myapplication.repository.SortBy
 
 @Composable
-fun SortByDD() {
+fun SortByDD(selectedOption: String, onSelectedOptionChanged: (String) -> Unit) {
+
     var sortByOptions = listOf(SortBy.CITY_NAME,
         SortBy.STADIUM_NAME,
         SortBy.SEATING_CAPACITY);
 
+    var expanded by remember { mutableStateOf(false) }
 
+    Column() {
+        Row() {
+            Icon(imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Sort DropDown",
+                modifier = Modifier.clickable { expanded = !expanded }
+            )
+            Text(text = selectedOption)
+        }
+        DropdownMenu(expanded = expanded,
+            onDismissRequest = { /*TODO*/ }) {
+
+            sortByOptions.forEach { option ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    onSelectedOptionChanged(option)
+                }) {
+                    Text(text = option)
+                }
+            }
+
+        }
+    }
 }
